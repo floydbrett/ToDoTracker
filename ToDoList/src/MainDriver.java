bdabf9754864cd1fd0073ab72062b3876c3555c2
floydbrett <51729027+floydbrett@users.noreply.github.com>
2024-03-15 19:14:55 -0500

    Update MainDriver.java
    Added text field to input tasks

--- ToDoList/src/MainDriver.java
+++ ToDoList/src/MainDriver.java
@@ -28,8 +28,19 @@ public class MainDriver {
 		
 		JPanel bodyPanel = new JPanel();
 		
+		bodyPanel.setLayout(new BorderLayout());
 		bodyPanel.setBackground(Color.lightGray);
 		bodyPanel.setPreferredSize(new Dimension(750,500));
+		
+		JLabel instruction = new JLabel();
+		instruction.setText("Input task names");
+		instruction.setFont(new Font("Times New Roman", Font.BOLD, 25));
+		bodyPanel.add(instruction, BorderLayout.NORTH);
+		
+		JTextField taskName = new JTextField();
+		taskName.setPreferredSize(new Dimension(200, 50));
+		
+		bodyPanel.add(taskName, BorderLayout.CENTER);
 		
 		frame.add(bodyPanel);
 		
